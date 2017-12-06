b408d88e61a854db50c7f5fe477d1d2ff4b9e755
Raul Buibas <raul.buibas@elektrobit.com>
2017-12-06 13:30:44 +0200

    Commit skeleton for Voyager's communication system. Initial implementation in Kotlin.

---
+++ Comm/src/voy/comm/chatengine/Dispatcher.kt
@@ -0,0 +1,7 @@
+package voy.comm.chatengine
+
+import voy.comm.starcalendar.*
+
+fun main(args: Array<String>) {
+    println("Captain's log, stardate" + getDummyStardate())
+}
---
+++ Comm/src/voy/comm/starcalendar/Stardate.kt
@@ -0,0 +1,6 @@
+package voy.comm.starcalendar
+
+fun getDummyStardate(): String
+{
+    return "51003.7"
+}
